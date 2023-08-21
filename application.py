5aca59f9ba5ba34a2a06b4267c663b04d93b30d7
Muskan022 <muskangupta120@gmail.com>
2023-08-22 00:41:35 +0530

    first commit

--- application.py
+++ application.py
@@ -2,41 +2,46 @@ from flask import Flask,request,jsonify,render_template
 import pickle 
 import pandas as pd 
 import numpy as np 
+import traceback
 from sklearn.preprocessing import StandardScaler
  
 
-app = Flask(__name__)
-
+application = Flask(__name__)
+app = application
 # import linear regressor model and scaler model pickle file
-standard_scaler = pickle.load(open(".vscode/models/scaler.pkl","rb")) 
-regressor = pickle.load(open(".vscode/models/linear_regressor.pkl","rb"))
+standard_scaler = pickle.load(open("models/scaler.pkl","rb")) 
+regressor_model = pickle.load(open("models/linear_regressor.pkl","rb"))
 
 # route for homepage
 @app.route('/')
 def index():
-    render_template("index.html")
+     return render_template("index.html")
 
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
-    if request.method == 'POST':
-        month = float(request.form.get('month'))
+  try:
+     if request.method == 'POST':
+        month = int(request.form.get('month'))
         Temperature = float(request.form.get('Temperature'))
         RH = float(request.form.get('RH'))
         Ws = float(request.form.get('Ws'))
         Rain = float(request.form.get('Rain'))
         FFMC = float(request.form.get('FFMC'))
         DMC = float(request.form.get('DMC'))
+        DC = float(request.form.get('DC'))
         ISI = float(request.form.get('ISI'))
         Classes = float(request.form.get('Classes'))
         Region = float(request.form.get('Region'))
 
-        scaled_data = standard_scaler.transform([[month,Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
-        result = regressor.predict(scaled_data)
+        scaled_data = standard_scaler.transform([[month,Temperature,RH,Ws,Rain,FFMC,DMC,DC,ISI,Classes,Region]])
+        result = regressor_model.predict(scaled_data)
 
-        return render_template('home.html',result = result[0])
-    else:
+        return render_template('home.html', result = result[0])
+     else:
         return render_template('home.html')
-    
+  except Exception as e:
+        traceback.print_exc()  # Print the exception to the console for debugging
+        return "An error occurred", 500
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
